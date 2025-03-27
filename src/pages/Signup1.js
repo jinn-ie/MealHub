225b07651192d32009cab3314da55064296c53d7
jinn-ie <lsj020724@naver.com>
2025-03-27 23:17:03 +0900

    유효성 검사

--- src/pages/Signup1.js
+++ src/pages/Signup1.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
 function Signup1() {
   const navigate = useNavigate();
 
-  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [formData, setFormData] = useState({ id: "", pwd: "" });
+  const [error, setError] = useState(null);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -12,8 +13,44 @@ function Signup1() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(formData);
+
+    // 유효성 검사
+
+    let validationError = "";
+    console.log(validationError);
+
+    if (!formData.id) {
+        validationError = "아이디를 입력해주세요.";
+    }
+
+    else if (!formData.pwd) {
+        validationError = "비밀번호를 입력해주세요.";
+    }
+
+    else if (!formData.checkpwd) {
+        validationError = "비밀번호 확인을 입력해주세요.";
+    }
+
+    else if (formData.pwd !== formData.checkpwd) {
+        validationError = "비밀번호가 일치하지 않습니다.";
+    }
+
+    if (Object.keys(validationError).length > 0) {
+        setError(validationError); // 에러 메시지 설정
+        return; // 에러가 있으면 제출하지 않음
+    }
+
     console.log("회원가입 데이터:", formData);
+    navigate("/signup2");
   };
+
+  useEffect(() => {
+    if(error!==null){
+      alert(error);
+      console.log("error:", error);
+    }
+  }, [error]);
 
   return (
     <div className="App Login">
@@ -25,7 +62,7 @@ function Signup1() {
             {/* <div className="form-group">
                 <label>e-mail</label>
                     <div className="input">
-                        <input type="email" name="username" onChange={handleChange} />
+                        <input type="email" name="id" onChange={handleChange} />
                         <p>@</p>
                         <input type="text" name="domain" list="domainList"></input>
                         <datalist id="domainList">
@@ -39,18 +76,21 @@ function Signup1() {
             </div> */}
             
             <div className="form-group">
-                <label>닉네임</label>
-                <input type="text" onChange={handleChange} />
+                <label>ID</label>
+                <div className="input-group">
+                <input type="text" className="id" name="id" onChange={handleChange} />
+                <button className="checkid">중복 확인</button>
+                </div>
             </div>
             <div className="form-group">
                 <label>비밀번호</label>
-                <input type="password" onChange={handleChange} />
+                <input type="password" name="pwd" onChange={handleChange} />
             </div>
             <div className="form-group">
                 <label>비밀번호 확인</label>
-                <input type="password" className="checkpwd" onChange={handleChange} />
+                <input type="password" name="checkpwd" onChange={handleChange} />
             </div>
-            <button className="submit" type="submit" onClick={() => navigate("/signup2")}>다음</button>
+            <button className="submit" type="submit">다음</button>
             <a onClick={() => navigate("/login")}>계정이 있으시다면? 로그인</a>
         </form>
     </div>
